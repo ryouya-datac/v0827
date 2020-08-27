20b69593a068ab918d5c5332ec98da1cd84a0c3c
pizza_girl <pizza.girl.dat@gmail.com>
2020-08-27 09:42:14 +0900

    復習

--- v0827/Form1.cs
+++ v0827/Form1.cs
@@ -12,9 +12,44 @@ namespace v0827
 {
     public partial class Form1 : Form
     {
+        int vx = -10;
+        int vy = -10;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+
+            label1.Left += vx;
+            label1.Top += vy;
+            if(label1.Left <0 )
+            {
+                vx = 10;
+            }
+
+          else if (label1.Top<0)
+            {
+                vy = 10;
+            }
+
+          else if (label1.Right>0)
+           {
+               vx = 10;
+           }
+
+          else if (label1.Bottom>0)
+            {
+                vx = 10;
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            
+            label2.Text = "" + mp.X + "," + mp.Y;
         }
     }
 }
