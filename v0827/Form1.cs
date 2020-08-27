4fcb95413221eb1dd4009c8ea71219d825c8713f
pizza_girl <pizza.girl.dat@gmail.com>
2020-08-27 11:12:58 +0900

    演習

--- v0827/Form1.cs
+++ v0827/Form1.cs
@@ -14,6 +14,9 @@ namespace v0827
     {
         int vx = -10;
         int vy = -10;
+        int point = 100;
+
+
         public Form1()
         {
             InitializeComponent();
@@ -21,35 +24,63 @@ namespace v0827
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-           
+            point--;
+            label3.Text = "Score " + point;
 
             label1.Left += vx;
             label1.Top += vy;
-            if(label1.Left <0 )
+
+            if (label1.Left < 0)
             {
-                vx = 10;
+                vx = Math.Abs(vx);
             }
 
-          else if (label1.Top<0)
+            if (label1.Right > ClientSize.Width)
             {
-                vy = 10;
+                vx = -Math.Abs(vx);
             }
 
-          else if (label1.Right>0)
-           {
-               vx = 10;
-           }
-
-          else if (label1.Bottom>0)
+            if (label1.Top < 0)
             {
-                vx = 10;
+                vy = Math.Abs(vy);
             }
+            if (label1.Bottom > ClientSize.Height)
+            {
+                    vy =-Math.Abs(vy);
+             }
+               
+            
+            // 2次元クラスPoint型の変数mpを宣言
+            Point mp = MousePosition;
+
+            // cposに、マウスのフォーム座標を取り出す。
+            //// MousePositionにマウス座標のスクリーン左上からのX、Yが入っている。
+            //// PointToClient()を使うと、スクリーン座標を、フォーム座標に変換できる。
+            mp = PointToClient(mp);
+
+            // ラベルに座標を表示
+            //// 変換したフォーム座標は、mp.X、mp.Yで取り出せる。
+            label2.Text = "" + mp.X + "," + mp.Y;
+            if (   (mp.X>=label1.Left)
+                && (mp.X<label1.Right)
+                && (mp.Y>=label1.Top)
+                && (mp.Y<label1.Bottom)
+                )
+            {
+                timer1.Enabled = false;
+            }
+            
         }
 
         private void label2_Click(object sender, EventArgs e)
         {
             
-            label2.Text = "" + mp.X + "," + mp.Y;
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {   
+                timer1.Enabled = false;          
         }
     }
 }
